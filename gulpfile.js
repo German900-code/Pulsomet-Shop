3821eb188eef7443bb68ead0afde732d96af967d
Герман Волошин <germanredmi13@gmail.com>
2025-03-11 18:07:38 +0200

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,62 @@ const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer').default;
 const cleanCSS = require("gulp-clean-css");
 
+// Запускаем локальный сервер
 gulp.task('server', function () {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 });
 
+// Компиляция SCSS в CSS + минификация
 gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min",
-        }))
+        .pipe(rename({ suffix: ".min" }))
         .pipe(autoprefixer())
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(gulp.dest("src/css"))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest("dist/css"))  // Кладём в dist/
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-    gulp.watch('src/*.html').on("change", browserSync.reload);
+// Копируем HTML в dist/
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+// Копируем JS в dist/
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
+});
+
+// Копируем изображения
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*")
+        .pipe(gulp.dest("dist/img"))
+        .pipe(browserSync.stream());
+});
+
+// Копируем шрифты
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"))
+        .pipe(browserSync.stream());
+});
+
+// Следим за изменениями и обновляем файлы
+gulp.task('watch', function () {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/*.html", gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js", gulp.parallel('scripts'));
+    gulp.watch("src/img/**/*", gulp.parallel('images'));
+    gulp.watch("src/fonts/**/*", gulp.parallel('fonts'));
+});
+
+// Запуск всех задач по умолчанию
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'images', 'fonts'));
