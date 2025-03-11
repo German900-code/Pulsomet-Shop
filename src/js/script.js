194e49a535ad72af097396ed564b060cd3122cf3
Герман Волошин <germanredmi13@gmail.com>
2025-03-11 14:39:11 +0200

    Исправлены пути к файлам для GitHub Pages

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,14 @@
 //         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"></button>',
 //         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"></button>',
 
+// const { name } = require("browser-sync");
+// const { emit } = require("gulp");
+
+// const browserSync = require("browser-sync").create();
+// const { name } = require("browser-sync");
+// const gulp = require("gulp");
+
+
 //         responsive: [
 //             {
 //                 breakpoint: 992,
@@ -69,4 +77,91 @@ $(function () {
         $('.catalog-item__content').eq(index).toggleClass('catalog-item__content_active');
         $('.catalog-item__list').eq(index).toggleClass('catalog-item__list_active');
     });
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+    // smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#'").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+    new WOW().init();
 });
+
+
+
