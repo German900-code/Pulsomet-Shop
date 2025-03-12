8eb21726fdc965ddc1aa763891d4cc833ab959b8
Adam7000-code <193603548+German900-code@users.noreply.github.com>
2025-03-12 15:34:50 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -46,8 +46,8 @@ $(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../../src/icons/chevron-left-solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../../src/icons/chevron-right-solid.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="./src/icons/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./src/icons/chevron-right-solid.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
